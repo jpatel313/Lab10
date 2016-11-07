bd6e22f98e7f79ce9fde8f1a4a5c5570d8ba8549
Jay Patel <jcp313@gmail.com>
2016-11-07 14:01:16 -0500

    Movie class + constructors

---
+++ Lab10new/Movie.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab10new
+{
+    class Movie
+    {
+        private string title;
+        private string category;
+
+        public string Title1
+        {
+            get
+            {
+                return title;
+            }
+
+            set
+            {
+                title = value;
+            }
+        }
+
+        public string Category1
+        {
+            get
+            {
+                return category;
+            }
+
+            set
+            {
+                category = value;
+            }
+        }
+
+        public Movie(string inputTitles, string inputCategories)
+        {
+        Title1 = inputTitles; Category1 = inputCategories;
+        }
+
+
+    }
+}
